91bcbe17a34a21efeccae9dea144900106cfbb29
Vaganeytor355 <133026269+Vaganeytor355@users.noreply.github.com>
2023-11-24 00:52:30 -0300

    pagina
    terciario

---
+++ carrucel.js
@@ -0,0 +1,30 @@
+let slideIndex = 0;
+
+function mostrarSlide(index) {
+    const slides = document.getElementsByClassName("slide");
+
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = index;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
+
+function cambiarSlide(n) {
+    mostrarSlide(slideIndex += n);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    mostrarSlide(slideIndex);
+    setInterval(function () {
+        cambiarSlide(1);
+    }, 3000);
+});
